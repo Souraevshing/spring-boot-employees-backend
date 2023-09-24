6f836528d989040c10a2bca6440e511590fda42e
Sourav Kumar <isauravshing@gmail.com>
2023-09-24 16:18:40 +0530

    Add files via upload

---
+++ src/main/java/com/spring/backend/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.spring.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://650fea04e70eae3733e9f38b--moonlit-llama-9803d6.netlify.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
